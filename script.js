0833c4e0c1d902d60bc951b1bb893017ba6d6554
Sycinz <gronbosof@gmail.com>
2022-10-18 20:46:18 +0200

    Ended project

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function getCredentials() {
-    var text = "The data has been saved."
     const user = document.querySelector('#user')
-    const passwd = document.querySelector('#passwd')
-    const repeat_passwd = document.querySelector('#repeat_passwd')
-    var credentials = new Array(user.value, passwd.value, repeat_passwd.value)
+    const psw = document.querySelector('#psw')
+    const repeat_psw = document.querySelector('#repeat_psw')
 
-    var blob = new Blob([text], { type: "text/plain;charset=utf-8" })
+    var credentials = user.value + "\n" + psw.value + "\n" + repeat_psw.value   
+
+    var blob = new Blob([credentials], { type: "text/plain;charset=utf-8" })
 
     saveAs(blob, "test.txt")
 
